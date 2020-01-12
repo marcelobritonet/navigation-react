f5fef7ce6b08d10c1efb77b2c4c8fab942f40f74
marcelobritonet <marcelobrito@marcelobrito.net>
2020-01-12 15:08:37 -0300

    Updates

---
+++ precache-manifest.629dee61ef3981329293651cdaf11f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd6551f46e928fb72cf941ccf2344b3",
+    "url": "/navigation-react/index.html"
+  },
+  {
+    "revision": "75795d882f60635b7add",
+    "url": "/navigation-react/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b0a7c4c142a98b4db55a",
+    "url": "/navigation-react/static/js/2.1c8f5f4d.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/navigation-react/static/js/2.1c8f5f4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75795d882f60635b7add",
+    "url": "/navigation-react/static/js/main.3a8c80a4.chunk.js"
+  },
+  {
+    "revision": "5b21c5f559342381d4bc",
+    "url": "/navigation-react/static/js/runtime-main.ab6cc759.js"
+  }
+]);
